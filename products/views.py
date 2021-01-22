e8caac6472121abf5f9d1616e3bdebec2af918b4
monish <monish@localhost.localdomain>
2021-01-22 23:41:58 +0530

    added changes and invoice to firebase data

--- products/views.py
+++ products/views.py
@@ -1224,13 +1224,14 @@ from firebase_admin import credentials, initialize_app, storage
 
 def file_upload_to_firebase(order_id,sub_id,token_id,file_path):
 	cred_json = BASE_DIR + "/crEdFilENoOneCanGuEsS78478883783FoldErName/cred.json"
-	if 'AppName' not in firebase_admin._apps:
-		cred = credentials.Certificate(cred_json)
-		initialize_app(cred, {'storageBucket': "essentials-e7555.appspot.com"},name="AppName")
-	else:
-		cred = credentials.Certificate(cred_json)
-		initialize_app(cred, {'storageBucket': "essentials-e7555.appspot.com"},name="AppName")
-		
+	cred = credentials.Certificate(cred_json)
+	initialize_app(cred, {'storageBucket': "essentials-e7555.appspot.com"})
+	try:
+		firebase_admin.delete_app('AppName')
+	except:
+		if 'AppName' not in firebase_admin._apps:
+			cred = credentials.Certificate(cred_json)
+			initialize_app(cred, {'storageBucket': "essentials-e7555.appspot.com"},name="AppName")	
 	fileName = file_path
 	bucket = storage.bucket()
 	blob = bucket.blob(fileName)
